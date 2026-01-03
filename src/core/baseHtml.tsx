9faa36fe3fac43def550a2a845860acb916d710b
Yuan Chiu <chyuaner@gmail.com>
2026-01-04 05:50:38 +0800

    feat: add footer

--- src/core/baseHtml.tsx
+++ src/core/baseHtml.tsx
@@ -212,13 +212,49 @@ export interface BaseData {
   [key: string]: any;
 }
 
+const Footer = () => {
+  const style = css `
+    padding: 0 1rem;
+    @media (min-width: 480px) {
+        div.right {
+            text-align: right;
+        }
+    }
+
+    p { margin: 0.5em 0; }
+    a {
+      text-decoration: none;
+      color: inherit;
+      transition: opacity 0.2s;
+      &:hover {
+        opacity: 0.7;
+      }
+    }
+  `;
+
+  return (<footer class={style}>
+    <div class={cx('container', gridClass)}>
+        <div class="col-8">
+            <p>© 2026 由 <a href="https://yuaner.tw" target={'_blank'}>Yuan Chiu</a> 製作，並採用 <a href="https://github.com/chyuaner/cloudflare-myip/blob/master/LICENSE" target={'_blank'}>MIT License</a> 授權</p>
+        </div>
+        <div class="col-4 right">
+            <p><a href="https://github.com/chyuaner/cloudflare-myip" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" /></svg>
+                View on Github
+            </a></p>
+        </div>
+    </div>
+  </footer>)
+}
+
 const Layout: FC<PropsWithChildren<{ title?: string, baseData?: BaseData }>> = (props) => {
   return (
     <Base title={props.title} baseData={props.baseData}>
-      <div id="main" class={cx(mainClass, mainStyle)}>
-        <div class={cx('container', gridClass, blockClass)}>
+      <div id="main" class={cx(mainClass)}>
+        <div class={cx('container', gridClass, blockClass, mainStyle)}>
             {props.children}
         </div>
+        <Footer />
       </div>
     </Base>
   )
